fe4c81fa6f323fccba8894e5ca9c2a7a2b3dcf6a
do5do <do151015@gmail.com>
2024-06-26 19:20:22 +0900

    style: 충전 api 주석 추가

--- src/main/java/com/jindo/minipay/account/checking/controller/CheckingAccountController.java
+++ src/main/java/com/jindo/minipay/account/checking/controller/CheckingAccountController.java
@@ -14,6 +14,11 @@ import org.springframework.web.bind.annotation.*;
 public class CheckingAccountController {
     private final CheckingAccountService checkingAccountService;
 
+    /**
+     * 충전하기
+     * @param request 계좌번호, 충전 금액
+     * @return 계좌번호, 남은 금액
+     */
     @PostMapping("/charge")
     public ResponseEntity<ChargeResponse> charge(
             @RequestBody @Valid ChargeRequest request) {
