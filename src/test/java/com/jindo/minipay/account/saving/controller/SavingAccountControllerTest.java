44aff83ec6270022561579c1e9d35dad2155e487
do5do <do151015@gmail.com>
2024-06-27 17:37:17 +0900

    test: 적금 계좌 납입 테스트 작성

--- src/test/java/com/jindo/minipay/account/saving/controller/SavingAccountControllerTest.java
+++ src/test/java/com/jindo/minipay/account/saving/controller/SavingAccountControllerTest.java
@@ -2,7 +2,10 @@ package com.jindo.minipay.account.saving.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jindo.minipay.account.saving.dto.CreateSavingAccountRequest;
+import com.jindo.minipay.account.saving.dto.PayInRequest;
+import com.jindo.minipay.account.saving.dto.PayInResponse;
 import com.jindo.minipay.account.saving.service.SavingAccountService;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -13,6 +16,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(SavingAccountController.class)
@@ -27,6 +31,7 @@ class SavingAccountControllerTest {
     ObjectMapper objectMapper;
 
     @Test
+    @DisplayName("적금 계좌를 생성한다.")
     void create() throws Exception {
         // given
         CreateSavingAccountRequest request = new CreateSavingAccountRequest(1L);
@@ -40,6 +45,28 @@ class SavingAccountControllerTest {
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(request)))
                 .andExpect(status().isCreated())
+                .andDo(print());
+    }
+
+    @Test
+    @DisplayName("적금 계좌에 납입한다.")
+    void payIn() throws Exception {
+        // given
+        String accountNumber = "8800-01-1234567";
+        PayInRequest request = new PayInRequest(accountNumber, 10000L);
+        PayInResponse response = new PayInResponse(accountNumber, 10000L);
+
+        given(savingAccountService.payIn(request))
+                .willReturn(response);
+
+        // when
+        // then
+        mockMvc.perform(post("/api/v1/account/saving/payin")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.accountNumber").value(accountNumber))
+                .andExpect(jsonPath("$.amount").value(10000))
                 .andDo(print());
     }
 }
--- src/test/java/com/jindo/minipay/account/saving/service/SavingAccountServiceTest.java
+++ src/test/java/com/jindo/minipay/account/saving/service/SavingAccountServiceTest.java
@@ -1,9 +1,12 @@
 package com.jindo.minipay.account.saving.service;
 
+import com.jindo.minipay.account.checking.entity.CheckingAccount;
 import com.jindo.minipay.account.common.exception.AccountException;
 import com.jindo.minipay.account.common.type.AccountType;
 import com.jindo.minipay.account.common.util.AccountNumberComponent;
 import com.jindo.minipay.account.saving.dto.CreateSavingAccountRequest;
+import com.jindo.minipay.account.saving.dto.PayInRequest;
+import com.jindo.minipay.account.saving.dto.PayInResponse;
 import com.jindo.minipay.account.saving.entity.SavingAccount;
 import com.jindo.minipay.account.saving.repository.SavingAccountRepository;
 import com.jindo.minipay.member.entity.Member;
@@ -20,7 +23,7 @@ import org.springframework.test.util.ReflectionTestUtils;
 
 import java.util.Optional;
 
-import static com.jindo.minipay.global.exception.ErrorCode.NOT_FOUND_MEMBER;
+import static com.jindo.minipay.global.exception.ErrorCode.*;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
@@ -42,17 +45,17 @@ class SavingAccountServiceTest {
     @InjectMocks
     SavingAccountService accountService;
 
+    String accountNumber = "8800-01-1234567";
+
+    Member member = Member.builder()
+            .email("test@test.com")
+            .password("test12345")
+            .name("tester1")
+            .build();
+
     @Nested
     @DisplayName("적금 계좌 생성 메서드")
     class CreateAccountMethod {
-        String accountNumber = "8888-01-1234567";
-
-        Member member = Member.builder()
-                .email("test@test.com")
-                .password("test12345")
-                .name("tester1")
-                .build();
-
         SavingAccount account = SavingAccount.of(accountNumber, member);
 
         CreateSavingAccountRequest request =
@@ -97,6 +100,65 @@ class SavingAccountServiceTest {
             assertThatThrownBy(() -> accountService.createAccount(request))
                     .isInstanceOf(AccountException.class)
                     .hasMessageContaining(NOT_FOUND_MEMBER.getMessage());
+        }
+    }
+
+    @Nested
+    @DisplayName("적금 계좌 납입 메서드")
+    class PayInMethod {
+        SavingAccount savingAccount = SavingAccount.of(accountNumber, member);
+
+        CheckingAccount checkingAccount =
+                CheckingAccount.of("8888-01-1234567", member);
+
+        PayInRequest request = new PayInRequest(accountNumber, 10000L);
+
+        @Test
+        @DisplayName("적금 계좌에 납입한다.")
+        void payIn() {
+            // given
+            ReflectionTestUtils.setField(checkingAccount, "balance", 10000L);
+            ReflectionTestUtils.setField(member, "checkingAccount", checkingAccount);
+
+            given(accountRepository.findByAccountNumberFetchJoin(accountNumber))
+                    .willReturn(Optional.of(savingAccount));
+
+            // when
+            PayInResponse response = accountService.payIn(request);
+
+            // then
+            assertEquals(10000L, response.amount());
+        }
+
+        @Test
+        @DisplayName("존재하지 않는 적금 계좌이면 예외가 발생한다.")
+        void payIn_notFound_accountNumber() {
+            // given
+            given(accountRepository.findByAccountNumberFetchJoin(accountNumber))
+                    .willReturn(Optional.empty());
+
+            // when
+            // then
+            assertThatThrownBy(() -> accountService.payIn(request))
+                    .isInstanceOf(AccountException.class)
+                    .hasMessageContaining(NOT_FOUND_ACCOUNT_NUMBER.getMessage());
+        }
+
+        @Test
+        @DisplayName("메인 계좌 잔액이 부족하면 예외가 발생한다.")
+        void payIn_insufficient_balance() {
+            // given
+            ReflectionTestUtils.setField(checkingAccount, "balance", 5000L);
+            ReflectionTestUtils.setField(member, "checkingAccount", checkingAccount);
+
+            given(accountRepository.findByAccountNumberFetchJoin(accountNumber))
+                    .willReturn(Optional.of(savingAccount));
+
+            // when
+            // then
+            assertThatThrownBy(() -> accountService.payIn(request))
+                    .isInstanceOf(AccountException.class)
+                    .hasMessageContaining(INSUFFICIENT_BALANCE.getMessage());
         }
     }
 }
