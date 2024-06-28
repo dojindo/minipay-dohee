311ce8ae4d485730330015ee5d12a2ec36435212
do5do <do151015@gmail.com>
2024-06-29 00:26:23 +0900

    refactor: InterruptedException 추가

--- src/main/java/com/jindo/minipay/lock/service/LockService.java
+++ src/main/java/com/jindo/minipay/lock/service/LockService.java
@@ -1,7 +1,7 @@
 package com.jindo.minipay.lock.service;
 
 public interface LockService {
-    void getLock(String key, Long waitTime, Long releaseTime);
+    void getLock(String key, Long waitTime, Long releaseTime) throws InterruptedException;
 
     void releaseLock(String key);
 }
