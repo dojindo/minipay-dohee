6fe001a072886e41bd2e08c2c47642b7096a4795
do5do <do151015@gmail.com>
2024-11-02 17:20:20 +0900

    test: 통합 테스트 수정 및 추가 - 계좌 동시성 테스트 수정 - 계좌 도메인 테스트 수정 - 거래 도메인 테스트 추가

--- src/test/java/com/jindo/minipay/integration/account/CheckingAccountIntegrationTest.java
+++ src/test/java/com/jindo/minipay/integration/account/CheckingAccountIntegrationTest.java
@@ -1,8 +1,6 @@
 package com.jindo.minipay.integration.account;
 
 import com.jindo.minipay.account.checking.dto.ChargeRequest;
-import com.jindo.minipay.account.checking.dto.RemitRequest;
-import com.jindo.minipay.account.checking.entity.CheckingAccount;
 import com.jindo.minipay.integration.BaseIntegrationTest;
 import com.jindo.minipay.member.entity.Member;
 import org.junit.jupiter.api.DisplayName;
@@ -10,12 +8,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 
-import java.util.Optional;
-
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @DisplayName("메인 계좌 통합테스트")
 class CheckingAccountIntegrationTest extends BaseIntegrationTest {
@@ -28,7 +22,7 @@ class CheckingAccountIntegrationTest extends BaseIntegrationTest {
         Member member = saveMember();
         saveCheckingAccount(member);
 
-        String accountNumber = "8888-01-1234567";
+        String accountNumber = member.getAccountNumber();
         ChargeRequest request = new ChargeRequest(accountNumber, 10000L);
 
         // when
@@ -43,56 +37,5 @@ class CheckingAccountIntegrationTest extends BaseIntegrationTest {
                 .body("accountNumber", equalTo(accountNumber))
                 .body("balance", equalTo(10000))
                 .log().all();
-    }
-
-    @Test
-    @DisplayName("메인 계좌에서 친구 계좌로 송금한다.")
-    void remit() {
-        // given
-        Member member = saveMember();
-        saveCheckingAccount(member);
-
-        saveFriendAndAccount();
-
-        String myAccountNumber = "8888-01-1234567";
-        String receiverAccountNumber = "8888-02-7654321";
-
-        RemitRequest request = RemitRequest.builder()
-                .myAccountNumber(myAccountNumber)
-                .receiverAccountNumber(receiverAccountNumber)
-                .amount(10000L)
-                .build();
-
-        // when
-        given().log().all()
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .body(request)
-                .when()
-                .post(URL + "/remit")
-                .then()
-                .statusCode(HttpStatus.OK.value())
-                .body("accountNumber", equalTo(myAccountNumber))
-                .body("balance", equalTo(0))
-                .log().all();
-
-        // then
-        Optional<CheckingAccount> receiverAccount =
-                checkingAccountRepository.findByAccountNumber(receiverAccountNumber);
-
-        assertTrue(receiverAccount.isPresent());
-        assertEquals(10000L, receiverAccount.get().getBalance());
-    }
-
-    void saveFriendAndAccount() {
-        Member member = Member.builder()
-                .email("friend@test.com")
-                .password("test12345")
-                .name("tester2")
-                .build();
-        memberRepository.save(member);
-
-        CheckingAccount account =
-                CheckingAccount.of("8888-02-7654321", member);
-        checkingAccountRepository.save(account);
     }
 }
