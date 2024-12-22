6f0a16f2f8b6fc71cd49f5ddf7a97ea7c7fd8f36
doheekim <do151015@gmail.com>
2024-12-22 14:06:37 +0900

    feat: firebase 의존성 및 설정 추가

---
+++ src/main/java/com/jindo/minipay/global/config/FirebaseConfig.java
@@ -0,0 +1,35 @@
+package com.jindo.minipay.global.config;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+
+import javax.annotation.PostConstruct;
+import java.io.IOException;
+import java.io.InputStream;
+
+@Slf4j
+@Configuration
+public class FirebaseConfig {
+    @Value(".key/minipay-firebase-key.json")
+    private ClassPathResource resource;
+
+    @PostConstruct
+    public void init() {
+        try {
+            InputStream serviceAccount = resource.getInputStream();
+
+            FirebaseOptions options = FirebaseOptions.builder()
+                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                    .build();
+
+            FirebaseApp.initializeApp(options);
+        } catch (IOException e) {
+            log.error("IOException is occurred. ", e);
+        }
+    }
+}
